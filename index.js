143351070a56c2e6f46b3ad2d3f34ae98a392b06
heejin99 <wkdgmlwls30@naver.com>
2021-09-06 23:37:13 +0900

    Todolist index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const session = require('express-session')
 const app = express()
 const bodyParser = require('body-parser')
+// const cors = require('cors')
+// const cookieParser = require('cookie-parser')
 
 const port = process.env.PORT || 3000
 
@@ -9,18 +12,16 @@ app.use(bodyParser.urlencoded({
 }))
 app.use(bodyParser.json());
 
-
-// view 경로
-app.set('views', __dirname + '/views');
-// 화면 engine - ejs
-app.set('view engine', 'ejs');
-
 // 기본 path
 app.use(express.static(__dirname + '/public'))
 
 // use routes
-app.use("/", require('./routes/index'))
+app.use("/api", require('./routes/main'))
+app.use("/api", require('./routes/signup'))
+app.use("/api", require('./routes/login'))
 
 app.listen(port, () => {
     console.log(`Server Listening on ${port}`)
 });
+
+
